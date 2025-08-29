9e6c416453b105e18efff9873ca0a6d8a8886066
UaRuairc <uaruairc@users.noreply.github.com>
2025-08-29 10:46:37 +0100

    chore(utils): add a util to time code block runtimes

--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,7 @@ import streamlit as st
 import logging, json, threading, inspect, time, gzip, hashlib, functools
 from streamlit.runtime.scriptrunner import get_script_run_ctx
 from collections import defaultdict
-
+from contextlib import contextmanager
 from src.config.config_management import ConfigManager
 
 default_style = "text-align: center; font-size: 3rem; font-weight: bold; width: 80px; margin: 0 auto; display: flex; align-items: center; justify-content: center; min-height: 80px;"
@@ -320,3 +320,12 @@ def timed(func):
         return result
 
     return wrapper
+
+@contextmanager
+def timed_block(name="Block"):
+    start = time.perf_counter()
+    try:
+        yield
+    finally:
+        end = time.perf_counter()
+        print(f"{name} took {end - start:.4f}s")
