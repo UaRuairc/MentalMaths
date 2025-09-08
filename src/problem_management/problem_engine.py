a705a3cce91624e333b8a0fa6223e642de6b376d
UaRuairc <uaruairc@users.noreply.github.com>
2025-09-08 12:14:18 +0100

    feat(problem_engine): add `Problem.tag_info()`
    - Import problem_tagger
    - Add `tag_info` on Problem class that calls and returns `problem_tagger()`

--- src/problem_management/problem_engine.py
+++ src/problem_management/problem_engine.py
@@ -5,6 +5,7 @@ from dataclasses import dataclass, field
 from typing import Union, Callable, Any
 from datetime import datetime, timezone
 import time
+from src.problem_management.problem_tagger import problem_tagger
 
 op_symbols = {
     "add": "+",
@@ -70,6 +71,9 @@ class Problem(ABC):
         self.operate()
         self.solved = True
 
+    @property
+    def tag_info(self):
+        return problem_tagger(self.left, self.right, self.answer, self.op, self.dtype)
     def operate(self) -> Any:
         pass
     def modify_problem(self) -> Any:
